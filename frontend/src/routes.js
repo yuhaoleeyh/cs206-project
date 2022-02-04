ae82d7834e9bf868fcad0bf2d6862897b87a076e
ridgedoug <richardyer@gmail.com>
2022-02-04 20:32:41 +0800

    rough hard coded job desc and reviews view

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -44,6 +44,7 @@ import RTL from "layouts/rtl";
 import Profile from "layouts/profile";
 import ProfileCreation from "layouts/profileCreation";
 import Recommender from "layouts/recommender";
+import JobDesc from "layouts/jobdesc";
 import SignIn from "layouts/authentication/sign-in";
 import SignUp from "layouts/authentication/sign-up";
 
@@ -110,6 +111,15 @@ const routes = [
     route: "/recommender",
     icon: <Settings size="12px" />,
     component: Recommender,
+    noCollapse: true,
+  },
+  {
+    type: "collapse",
+    name: "Job Desc",
+    key: "jobDesc",
+    route: "/jobdesc",
+    icon: <Settings size="12px" />,
+    component: JobDesc,
     noCollapse: true,
   },
   { type: "title", title: "Account Pages", key: "account-pages" },
